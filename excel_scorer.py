2b209816b412ca7cbc9f4363be404ea9c184c042
Alexia Zhou <alexiazhou726@ucla.edu>
2024-10-09 15:00:00 +0800

    allow subfolders

--- excel_scorer.py
+++ excel_scorer.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from readability import Readability
 import os
+import warnings
 
+# suppress UserWarnings (for SMOG warnings)
+warnings.filterwarnings("ignore", category=UserWarning)
 
-def process_single_file(input_file):
+def process_single_file(input_file, output_file):
+
     df = pd.read_excel(input_file)
-    input_file_base = os.path.splitext(os.path.basename(input_file))[0]
-    output_file = os.path.join(output_folder, f'{input_file_base}_readability_scores.xlsx')
 
     fres_scores = []
     fkgl_scores = []
@@ -34,18 +36,42 @@ def process_single_file(input_file):
 
     df.to_excel(output_file, index=False)
 
-    print(f"Readability scores saved to {output_file}")
 
-
-input_folder = 'input/'
-output_folder = 'output/'
+input_folder = 'readability_eval/'
+output_folder = 'readability_eval_results/'
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-for file_name in os.listdir(input_folder):
-    if file_name.endswith('.xlsx'):
-        input_file = os.path.join(input_folder, file_name)
-        process_single_file(input_file)
+for dirpath, dirnames, filenames in os.walk(input_folder):
+    
+    for file_name in filenames:
+
+        # skip temporary or hidden files (starting with `~` or `.`)
+        if file_name.startswith('~') or file_name.startswith('.'):
+            print(f"Skipping temporary file: {file_name}")
+            continue
+
+        if file_name.endswith('.xlsx'):
+            input_file = os.path.join(dirpath, file_name)
+            # input_file_base = os.path.splitext(os.path.basename(input_file))[0]
+            # output_file = os.path.join(output_folder, f'{input_file_base}_readability.xlsx')
+            output_file_name = file_name.replace('_Empty', '')
+
+            relative_path = os.path.relpath(dirpath, input_folder)
+            output_dir = os.path.join(output_folder, relative_path)
+
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            output_file = os.path.join(output_dir, output_file_name)
+
+            print(f"Processing {input_file}...")
+
+            try:
+                process_single_file(input_file, output_file)
+                print(f"    Succeeded. Saved to {output_file}")
+            except Exception as e:
+                print(f"    Failed. {e}")
 
 print("Processing completed for all files.")
